17570fa056211dccb8d3a9c71ab9c59625a65e53
thermatk <thermatk@thermatk.com>
2016-10-23 00:54:49 +0200

    for #5, but is a totally different approach altogether

--- app/src/main/java/com/thermatk/android/xf/telegram/Xposed.java
+++ app/src/main/java/com/thermatk/android/xf/telegram/Xposed.java
@@ -1,30 +1,45 @@
 package com.thermatk.android.xf.telegram;
 
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.amulyakhare.textdrawable.TextDrawable;
+import android.content.res.XModuleResources;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
-import static de.robv.android.xposed.XposedHelpers.findClass;
-import static de.robv.android.xposed.XposedHelpers.getStaticIntField;
 
-public class Xposed implements IXposedHookLoadPackage {
+public class Xposed implements IXposedHookZygoteInit,IXposedHookLoadPackage {
+    static String MODULE_PATH;
+
+    @Override
+    public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
+        MODULE_PATH = startupParam.modulePath;
+    }
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         if (!"org.telegram.messenger".equals(loadPackageParam.packageName)) {
             return;
         }
+        findAndHookMethod("android.content.res.AssetManager", loadPackageParam.classLoader,
+                "open", String.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            String path = (String)param.args[0];
+                            if (path.contains("emoji/v10_emoji2.0x")) {
+                                XposedBridge.log("TGEMOJI loading replacement "+path);
 
+                                param.setResult(XModuleResources.createInstance(MODULE_PATH, null).getAssets().open("GoogleNoto/"+path.split("x_")[1]));
+                                return;
+                            }
+                    }
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    }
+                });
+        // CODE FROM V.1.1
+        /*
         final Class<?> EmojiClass = findClass("org.telegram.messenger.Emoji", loadPackageParam.classLoader);
 
 
@@ -74,5 +89,6 @@ public class Xposed implements IXposedHookLoadPackage {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        */
     }
 }
