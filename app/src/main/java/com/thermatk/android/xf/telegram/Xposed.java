94d37be65150da121d3e23d32b90cd724e6c5998
thermatk <thermatk@thermatk.com>
2016-10-23 16:05:07 +0200

    delete old code

--- app/src/main/java/com/thermatk/android/xf/telegram/Xposed.java
+++ app/src/main/java/com/thermatk/android/xf/telegram/Xposed.java
@@ -38,57 +38,5 @@ public class Xposed implements IXposedHookZygoteInit,IXposedHookLoadPackage {
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     }
                 });
-        // CODE FROM V.1.1
-        /*
-        final Class<?> EmojiClass = findClass("org.telegram.messenger.Emoji", loadPackageParam.classLoader);
-
-
-        XC_MethodReplacement replaceEmoji_methodReplacement = new XC_MethodReplacement() {
-            @Override
-            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                return methodHookParam.args[0];
-            }
-        };
-        findAndHookMethod(EmojiClass,
-                "replaceEmoji", CharSequence.class, Paint.FontMetricsInt.class, int.class, boolean.class, int[].class,
-                replaceEmoji_methodReplacement);
-
-        try {
-            final Class<?> EmojiGridAdapter = findClass("org.telegram.ui.Components.EmojiView.EmojiGridAdapter", loadPackageParam.classLoader);
-
-            findAndHookMethod(EmojiGridAdapter, "getView", int.class, View.class, ViewGroup.class,
-                    new XC_MethodReplacement() {
-                        @Override
-                        protected Object replaceHookedMethod(final MethodHookParam methodHookParam) throws Throwable {
-                            Object o = XposedBridge.invokeOriginalMethod(methodHookParam.method, methodHookParam.thisObject, methodHookParam.args);
-                            if (o instanceof ImageView) {
-                                ImageView imageView = (ImageView) o;
-                                String convert = (String) imageView.getTag();
-                                try {
-
-                                    int bigImgSize = getStaticIntField(EmojiClass, "bigImgSize");
-                                    imageView.setImageDrawable(TextDrawable.builder()
-                                            .beginConfig()
-                                            .textColor(Color.BLACK)
-                                            .fontSize(bigImgSize)
-                                            .endConfig()
-                                            .buildRect(convert, Color.TRANSPARENT));
-                                } catch (Exception ignored) {
-                                    imageView.setImageDrawable(TextDrawable.builder()
-                                            .beginConfig()
-                                            .textColor(Color.BLACK)
-                                            .endConfig()
-                                            .buildRect(convert, Color.TRANSPARENT));
-
-                                }
-
-                            }
-                            return o;
-                        }
-                    });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        */
     }
 }
